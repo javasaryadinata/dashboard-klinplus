356e621eea0cda7b789bf6f69db46679228aac49
Javas <javasaryadinata24@gmail.com>
2025-06-17 14:30:09 +0700

    Update bookingForm.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import "./bootstrap";
 import Alpine from "alpinejs";
-import BookingForm from "./bookingForm";
+import BookingForm from "./BookingForm";
 
 window.Alpine = Alpine;
 Alpine.data("BookingForm", BookingForm);
