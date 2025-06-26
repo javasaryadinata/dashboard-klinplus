06b7a49580698a491521f442e7ab297f893d80bf
Javas <javasaryadinata24@gmail.com>
2025-06-26 12:31:39 +0700

    Update email invoice footer

--- resources/views/orders/invoice_pdf.blade.php
+++ resources/views/orders/invoice_pdf.blade.php
@@ -6,20 +6,23 @@
     <style>
         body {
             font-family: DejaVu Sans, sans-serif;
-            font-size: 14px;
+            font-size: 16px;
             padding: 40px;
         }
         .header {
-            margin-bottom: 20px;
+            margin-bottom: 30px;
         }
         .header h2 {
             margin: 20px 0 20px 0
         }
-        .alamat {
+        .contact {
             text-align: right;
         }
         p {
-            margin-bottom: 2px;
+            margin: 4px 0 0 0; 
+        }
+        .status {
+            color: green;
         }
         table {
             width: 100%;
@@ -39,20 +42,28 @@
         .total {
             font-weight: bold;
         }
+        .footer {
+            font-size: 14px;
+        }
     </style>
 </head>
 <body>
     <div class="header">
         <h2>LOGO</h2>
-        <p class="alamat">
-            Jl. Ngagel Jaya Utara No.152<br>
-            Baratajaya, Kec. Gubeng, Surabaya, Jawa Timur 60284
+        <p class="contact">
+            Jl. Ngagel Jaya Utara No.152, Baratajaya<br>
+            Kec. Gubeng, Surabaya, Jawa Timur 60284
         </p>
+        <p class="contact">+6281331155778</p>
         <h2>INVOICE</h2>
         <p>ID Order : <b>{{ $order->id_order }}</b></p>
         <p>Pelanggan : {{ $order->pelanggan->nama_pelanggan }}</p>
         <p>Alamat : {{ $order->alamat_lokasi }}</p>
         <p>Tanggal Pengerjaan : {{ \Carbon\Carbon::parse($order->tanggal_pengerjaan)->format('d-m-Y') }}</p>
+        <p>Status : <b><span>{{ $order->metode_pembayaran }}</b></span></p>
+    </div>
+    <div>
+        <p><b>Detail Layanan :</b></p>
     </div>
     <table>
         <thead>
@@ -82,6 +93,18 @@
             </tr>
         </tfoot>
     </table>
-    <p style="margin-top:30px;"><i>Terima kasih telah menggunakan layanan kami.</i></p>
+    <div class="footer">
+        <p style="margin-top: 20px;">Keterangan :</p>
+        <p>1. Mohon melakukan pembayaran uang muka/DP sebesar 50% dari total tagihan, guna memproses booking anda. Bukti pembayaran dapat dikirimkan melalui whatsapp, dan pelunasan akan dilakukan sesuai kesepakatan.</p>
+        <p>2. Reschedule/ganti tanggal pengerjaan dapat dilakukan dengan ketentuan maksimal H-1 sebelum tanggal pengerjaan awal.</p>
+        <p style="margin-top: 20px;">Pembayaran dapat ditransfer ke rekening berikut :</p>
+        <p style="margin-top: 10px;"><b>BCA</b></p>
+        <p>No. Rekening : <span><b>12345678</b></span></p>
+        <p>Atas Nama : <span><b>John Doe</b></span></p>
+        <p style="margin-top: 10px;">
+            Mohon konfirmasi setelah melakukan transfer melalui whatsapp, disertai dengan bukti pembayaran.
+        </p>
+        <p style="margin-top: 30px;"><i>Terima kasih telah menggunakan layanan Klinplus.</i></p>    
+    </div>
 </body>
 </html>
